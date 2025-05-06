6ace0aca1af7db126f0ed958fc7624fdf9f92f1f
toast <levirooth29@gmail.com>
2025-05-06 09:11:02 +0200

    qwd

--- main.js
+++ main.js
@@ -1339,7 +1339,7 @@ Game.Launch=function()
 
       
     '</div><div class="subsection update">'+
-	'<div class="title">06/05/2025 - GROTE UPDATE!!! (Alle updates staan in volgorde)</div>'+
+	'<div class="title">06/05/2025 - GROTE UPDATE!!!! (Alle updates staan in volgorde)</div>'+
 	'<div class="listing">&bull; Jan Eradus & Banaan Joran toevegoegd, Ideeën? info@tinekesieders.nl</div>'+
 	'<div class="listing">&bull; Nieuwe namen voor omas.</div>'+
 	'<div class="listing">&bull; Op de koekje klikken krijg je nu tinekes, nog niet allemaal. </div>'+
